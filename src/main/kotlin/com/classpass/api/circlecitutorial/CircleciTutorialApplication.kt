d77bc808819f23e9f5ab1be4e42785ad51607112
Troy <yyzgmmyyd@gmail.com>
2022-06-07 21:22:09 +0800

    initial commit

---
+++ src/main/kotlin/com/classpass/api/circlecitutorial/CircleciTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.classpass.api.circlecitutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CircleciTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<CircleciTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/classpass/api/circlecitutorial/CircleciTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.classpass.api.circlecitutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircleciTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
